f250d290f6849eaa868bd334bd2f9e7eaf1f6730
ragav7332 <96164910+ragav7332@users.noreply.github.com>
2022-10-17 22:33:06 +0530

    Delete IBM-Project-31235-1660197981-main directory

--- IBM-Project-31235-1660197981-main/Assignment/Sriram K/Assignment 1/sqlite_db_setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('User_database.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE users (email TEXT, username TEXT, rollnumber INTEGER, pin INTEGER)')
-print("Table created successfully")
-conn.close()
