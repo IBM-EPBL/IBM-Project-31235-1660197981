500e47ef5cbff1b34dcf04149372da382138e5bd
ragav7332 <96164910+ragav7332@users.noreply.github.com>
2022-11-24 13:47:30 +0530

    Delete IBM-Project-31235-1660197981-main directory

--- IBM-Project-31235-1660197981-main/Assignment/Sriram K/Assignment 1/models.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-def retrieveUsers():
-    con = sql.connect("User_database.db")
-    cur = con.cursor()
-    cur.execute("SELECT username, pin FROM users")
-    users = cur.fetchone()
-    con.close()
-    return users
